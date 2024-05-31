098df99b1282b84de411071992c2215e4ef426e8
Your Name <dulexw@gmail.com>
2024-05-31 03:14:26 +0100

    Finished working on the some parts of front and backend

---
+++ face-scanner-frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Face Scanner</h1>
+                <img src="http:127.0.0.1:5000/video_feed" alt="Face Scanner" />
+            </header>
+        </div>
+    );
+}
