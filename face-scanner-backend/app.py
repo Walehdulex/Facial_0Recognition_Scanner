73db96636f3cf32e82582bb43c49d28ffc721762
Your Name <dulexw@gmail.com>
2024-05-31 17:49:21 +0100

    Modifying App.py to implement the login and logout feature

--- face-scanner-backend/app.py
+++ face-scanner-backend/app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response,redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import cv2
 import face_recognition
 import pickle
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 #Load known face encodings and names
 
@@ -13,6 +21,11 @@ with open('face_encodings.pkl', 'rb') as f:
 # Access webcam
 
 video_capture = cv2.VideoCapture(0)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
 
 def generate_frames():
     while True:
@@ -39,6 +52,26 @@ def generate_frames():
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('index'))
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 @app.route('/')
 def index():
